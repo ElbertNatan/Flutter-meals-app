2f5549933c70bdddb1086dd8a22cf45602fa2739
Elbert <elbertnatan@hotmail.com>
2023-08-08 10:25:00 -0300

    - melhorando filtros

--- flutter_meals_app/lib/screens/filters.dart
+++ flutter_meals_app/lib/screens/filters.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+/*import 'package:flutter_meals_app/screens/tabs.dart';
+import 'package:flutter_meals_app/widgets/main_drawer.dart';*/
 
 class FiltersScreen extends StatefulWidget {
   const FiltersScreen({super.key});
@@ -18,6 +20,18 @@ class _FiltersScreenState extends State<FiltersScreen> {
       appBar: AppBar(
         title: const Text('Filters'),
       ),
+      /*drawer: MainDrawer(
+        onSelectScreen: (identifier) {
+          Navigator.of(context).pop();
+          if (identifier == 'meals') {
+            Navigator.of(context).push(
+              MaterialPageRoute(
+                builder: (ctx) => const TabsScreen(),
+              ),
+            );
+          }
+        },
+      ),*/
       body: Column(
         children: [
           SwitchListTile(
