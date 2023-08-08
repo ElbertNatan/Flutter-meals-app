e8883f8e8ad0fd2af7f19d5bc5f470eb3060c005
Elbert <elbertnatan@hotmail.com>
2023-08-08 11:27:40 -0300

    - usando provider favorito

--- flutter_meals_app/lib/screens/meal_datails.dart
+++ flutter_meals_app/lib/screens/meal_datails.dart
@@ -1,19 +1,35 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_meals_app/models/meal.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_meals_app/providers/favorites_provider.dart';
 
-class MealDetailsScreen extends StatelessWidget {
+class MealDetailsScreen extends ConsumerWidget {
   const MealDetailsScreen({super.key, required this.meal});
 
   final Meal meal;
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
     return Scaffold(
       appBar: AppBar(
         title: Text(meal.title),
         actions: [
-          IconButton(onPressed: (){onToggleFavorite(meal);}, icon: const Icon(Icons.star)),
+          IconButton(
+              onPressed: () {
+                final wasAdded = ref
+                    .read(favoriteMealsProvider.notifier)
+                    .toggleMealFavoriteStatus(meal);
+                ScaffoldMessenger.of(context).clearSnackBars();
+                ScaffoldMessenger.of(context).showSnackBar(
+                  SnackBar(
+                    content: Text(wasAdded
+                        ? 'Meal is now a favorite'
+                        : 'Meal is no longer a favorite'),
+                  ),
+                );
+              },
+              icon: const Icon(Icons.star)),
         ],
       ),
       body: SingleChildScrollView(
@@ -65,10 +81,8 @@ class MealDetailsScreen extends StatelessWidget {
               child: Text(
                 step,
                 textAlign: TextAlign.center,
-                style: Theme.of(context)
-                    .textTheme
-                    .bodyMedium!
-                    .copyWith(color: Theme.of(context).colorScheme.onBackground),
+                style: Theme.of(context).textTheme.bodyMedium!.copyWith(
+                    color: Theme.of(context).colorScheme.onBackground),
               ),
             ),
         ]),
